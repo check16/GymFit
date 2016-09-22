35d2957b49158e7eebef0b92346ddf3b64729df4
Antonio <antonio.san.87@hotmail.com>
2016-09-22 19:58:41 +0200

    Commit inicial de la aplicación con la estructura basica del proyecto GymFit

---
+++ src/main/java/com/asanast/gymfit/GymfitDawApplication.java
@@ -0,0 +1,12 @@
+package com.asanast.gymfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymfitDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymfitDawApplication.class, args);
+	}
+}
