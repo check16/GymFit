dca72aa9192a918f915279dadc62c4807c9c78ea
Antonio <antonio.san.87@hotmail.com>
2016-10-10 21:12:39 +0200

    Agregado al pom.xml la dependencia de Spring Security

--- src/test/java/com/asanast/gymfit/GymfitDawApplicationTests.java
+++ src/test/java/com/asanast/gymfit/GymfitDawApplicationTests.java
@@ -1,9 +1,6 @@
 package com.asanast.gymfit;
 
-import java.math.BigDecimal;
-import java.util.Date;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,10 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.asanast.gymfit.dao.PesoDao;
-import com.asanast.gymfit.dao.RolDao;
-import com.asanast.gymfit.dao.UsuarioDao;
-import com.asanast.gymfit.pojo.Peso;
 import com.asanast.gymfit.pojo.Usuario;
 import com.asanast.gymfit.service.UsuarioService;
 
@@ -24,30 +17,19 @@ import com.asanast.gymfit.service.UsuarioService;
 public class GymfitDawApplicationTests {
 	
 	@Autowired
-	private UsuarioDao usuarioDao;
-	
-	@Autowired
-	private PesoDao pesoDao;
-
-	SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+	private UsuarioService usuarioService;
 	
 	@Test
 	@Transactional
 	public void contextLoads() throws ParseException {
 		
-		Usuario usuario = usuarioDao.findByLogin("pepito");
+		Usuario usuario = new Usuario("silmaro", "1234","silmaro@prueba.es", 26, 170, "img/usuario-icono.png");
 		
-		Peso peso2 = pesoDao.findByUsuarioAndFecha(usuario, new Date(format.parse("24-09-2016").getTime()));
+		usuarioService.save(usuario);
 		
-		if(peso2!=null) {
-			peso2.setPeso(new BigDecimal("200.71"));
-			pesoDao.update(peso2);
-			System.out.println("Peso ACTUALIZADO CORRECTAMENTE");
-		}else {
-			Peso peso3 = new Peso(new BigDecimal("60"), new Date(format.parse("24-09-2016").getTime()), usuario);
-			pesoDao.save(peso3);
-			System.out.println("Peso GRABADO CORRECTAMENTE");
-		}
+		System.out.println("ALMACENADO");
+		
+		
 	}
 
 }
