cedce1fc3d173cdc43350b7e60663a83703d7645
Antonio <antonio.san.87@hotmail.com>
2016-10-12 21:47:59 +0200

    Corregido los nombres de usuario en la seccion del Home

--- src/main/java/com/asanast/gymfit/pojo/Usuario.java
+++ src/main/java/com/asanast/gymfit/pojo/Usuario.java
@@ -143,13 +143,29 @@ public class Usuario {
 		this.rol = rol;
 	}
 
-	@Override
-	public String toString() {
-		return "Usuario [idUsuario=" + idUsuario + ", login=" + login + ", clave=" + clave + ", email=" + email
-				+ ", edad=" + edad + ", altura=" + altura + ", rutaFoto=" + rutaFoto + ", rol=" + rol + ", pesos="
-				+ pesos + ", entrenamientos=" + entrenamientos + "]";
-	}	
-	
-	
+	public String getApellido1() {
+		return apellido1;
+	}
 
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}	
+
+	
 }
